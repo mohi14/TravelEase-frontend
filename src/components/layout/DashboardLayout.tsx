42c2d1eea5ab699474b7d96cc06e02e163a541a2
mohi14 <mohiuddinkhan1409@gmail.com>
2026-07-02 20:27:50 +0200

    refactor(ui): dashboard layout ui fixing

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,17 +1,21 @@
-import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "../ui/sidebar";
 
 import { Outlet } from "react-router";
 import { AppSidebar } from "./AppSidebar";
-
+import { Separator } from "../ui/separator";
 
 export default function DashboardLayout() {
   return (
-      <SidebarProvider>
+    <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-       <Outlet/>
-      </main>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <Outlet />
+        </div>
+      </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
